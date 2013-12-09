860b7d16bbd862c5654e12d36e5c92abf1663d08
Camille Troillard <camille@osculator.net>
2013-12-09 11:24:57 +0100

    Simplify the CL compile script a bit.

--- setup/compile.lisp
+++ setup/compile.lisp
@@ -1,5 +1,6 @@
-(in-package :cl-user)
+(in-package "CL-USER")
 
+(defvar *saved-executable-name* "cl-app")
 (defvar *build-dir* (pathname-directory (pathname (concatenate 'string (getenv "BUILD_DIR") "/"))))
 (defvar *cache-dir* (pathname-directory (pathname (concatenate 'string (getenv "CACHE_DIR") "/"))))
 (defvar *buildpack-dir* (pathname-directory (pathname (concatenate 'string (getenv "BUILDPACK_DIR") "/"))))
@@ -17,39 +18,15 @@
 	(funcall (symbol-function (find-symbol "INSTALL" (find-package "QUICKLISP-QUICKSTART")))
 		 :path (make-pathname :directory (pathname-directory ql-setup))))))
 
-(asdf:clear-system "acl-compat")
-
-;;; Load all .asd files in the repos subdirectory.  The compile script puts
-;;; several systems in there, because we are using versions that are 
-;;; different from those in Quicklisp. (update: Can't just load the files apparently,
-;;; have to add dirs to asdf:*central-registry*.  Blah.
-(let* ((asds (directory (make-pathname :directory  (append *cache-dir* '( "repos" :wild-inferiors))
-				       :name :wild
-				       :type "asd")))
-       (directories (remove-duplicates (mapcar #'pathname-directory asds) :test #'equal)))
-  (dolist (d directories)
-    (push (make-pathname :directory d) asdf:*central-registry*)))
-
-;;; App can redefine this to do runtime initializations
-(defun initialize-application ()
-  )
-
-;;; Default toplevel, app can redefine.
+;;; Default toplevel, app must redefine.
 (defun heroku-toplevel ()
-  (initialize-application)
-  ;; Start the web server
-  (let ((port (parse-integer (getenv "PORT"))))
-    (format t "Listening on port ~A~%" port)
-    (funcall (symbol-function (find-symbol "START" (find-package "NET.ASERVE")))
-	     :port port)
-    (loop (sleep 60))			;sleep forever
-    ))
+  (loop (sleep 60)))
 
 ;;; Load the application from sources
 (load (make-pathname :directory *build-dir* :defaults "heroku-setup.lisp"))
 
 ;;; Save the application as an image
-(let ((app-file (format nil "~A/lispapp" (getenv "BUILD_DIR")))) ;must match path specified in bin/release
+(let ((app-file (format nil "~A/~A" (getenv "BUILD_DIR") *saved-executable-name*))) ;must match path specified in bin/release
   (format t "Saving to ~A~%" app-file)
   (save-application app-file
 		    :prepend-kernel t
